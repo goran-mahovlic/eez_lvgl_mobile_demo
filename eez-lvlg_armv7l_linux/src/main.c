4b364276929862225316d3d46c969f5ae78367ef
Goran Mahovlic <goran.mahovlic@gmail.com>
2024-05-19 07:04:14 +0200

    Fixing build

--- eez-lvlg_armv7l_linux/src/main.c
+++ eez-lvlg_armv7l_linux/src/main.c
@@ -34,8 +34,8 @@ int main(void)
     lv_disp_drv_init(&disp_drv);
     disp_drv.draw_buf   = &disp_buf;
     disp_drv.flush_cb   = fbdev_flush;
-    disp_drv.hor_res    = 800;
-    disp_drv.ver_res    = 480;
+    disp_drv.hor_res    = 500;
+    disp_drv.ver_res    = 960;
     lv_disp_drv_register(&disp_drv);
 
     evdev_init();
